97647650ec2cf3533d5596136263d9d902346ac0
KhaiTrn <khai.tran.srp@gmail.com>
2025-03-20 09:55:07 +0700

    6

--- my-media/server.js
+++ my-media/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { DataTypes, Sequelize } from "sequelize";
 
 const app = express();
 
@@ -51,6 +52,107 @@ app.get(`/headers`, (request, response, next) => {
   console.log(request.headers);
   response.json(`Header Parameters`);
 });
+
+const sequelize = new Sequelize(
+  "mysql://root:123456@localhost:3306/demo_database"
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(`kết nối với db thành công`);
+  })
+  .catch(() => {
+    console.log(`kết nối với db không thành công`);
+  });
+
+// try {
+//    await sequelize.authenticate();
+//    console.log(`Kết nối với db thành công`);
+// } catch (error) {
+//    console.log(`Kết nối với db KHÔNG thành công`);
+// }
+
+/**
+ * Code first
+ * Đi từ code tạo ra table
+ *    tạo table bằng code javascript
+ */
+// tạo ra model từ define
+const Cars = sequelize.define(
+  "Cars",
+  {
+    car_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    passengers: {
+      type: DataTypes.INTEGER,
+    },
+    max_speed: {
+      type: DataTypes.STRING,
+    },
+    gearbox_type: {
+      type: DataTypes.STRING,
+    },
+    fuel_type: {
+      type: DataTypes.STRING,
+    },
+    price_per_day: {
+      type: DataTypes.DOUBLE,
+    },
+    discount_percentage: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: `cars`,
+    timestamps: false,
+  }
+);
+Cars.sync()
+  .then(() => {
+    console.log(`Đồng bộ table cars thành công`);
+  })
+  .catch(() => {
+    console.log(`Đồng bộ table cars KHÔNG thành công`);
+  });
+
+app.get(`/cars`, async (req, res, next) => {
+  // const cars = await sequelize.query(`SELECT * FROM cars`);
+
+  const cars = await Cars.findAll({ raw: true });
+  console.log({ cars });
+
+  // console.log({ cars });
+  // console.log(`cars - 1`, cars[0]);
+  // // console.log(`cars`, cars);
+
+  res.json(cars);
+});
+
+/**
+ * Database first
+ * Đi từ câu lệnh SQL để tạo ra table
+ *    - tạo table bằng câu lệnh SQL
+ */
 
 app.listen(3069, () => {
   console.log(`Server Online At Port 3069`);
